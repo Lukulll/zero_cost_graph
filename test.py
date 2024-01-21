971f430f5ffa8044ac7bfc1e85d51f151fdd8c29
Luc Truong <92753286+Lukulll@users.noreply.github.com>
2024-01-21 11:48:43 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from model.NAS_Bench_Graph_model import Net,get_NAS_Bench_graph_HP,get_num_features,get_num_classes
 from pruners.predictive import find_measures
+from utils.weight_initializers import init_net
 
 if __name__ == '__main__':
   operations = ["gat", "gcn", "gin", "cheb"]
@@ -10,7 +11,8 @@ if __name__ == '__main__':
   device = 'cpu'
   hp = get_NAS_Bench_graph_HP()
   model = Net(operations, link, hp, in_dim, out_dim, dname).to(device)
-
+  init_net(model,'kaiming','zero')
   measure = 'synflow'
+  
   print(find_measures(model,dname,device, measure_names = [measure]))
 
